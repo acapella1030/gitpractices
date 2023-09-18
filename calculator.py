92c6a1bae5810a16fa1be509de6550c94c5120dc
André Capella <andre.capella1030@gmail.com>
2023-09-18 02:58:30 -0600

    My first commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# Solicita al usuario ingresar dos números
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Realiza la suma
+suma = numero1 + numero2
+
+# Muestra el resultado
+print("La suma de {} y {} es igual a: {}".format(numero1, numero2, suma))
+
+
+
